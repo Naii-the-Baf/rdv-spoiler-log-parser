a8f8567740095a22d39195decb0cc8053d66cce5
Naii-the-Baf <sfabian200@gmail.com>
2024-12-04 11:02:02 -0600

    Override SIGINT and SIGTERM behaviors with the defaults

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
-from PySide6 import QtWidgets
+import signal
+from PySide6 import QtCore, QtWidgets, QtGui
 from gui.main_window import MainWindow
 from arg_parser import ArgParser
 
@@ -8,7 +9,10 @@ if __name__ == "__main__":
     
     parser = ArgParser()
     args = parser.get_args()
-
+    
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    signal.signal(signal.SIGTERM, signal.SIG_DFL)
+    
     widget = MainWindow(args.file)
     widget.resize(600, 600)
     widget.show()
