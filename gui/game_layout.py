340609685eae809897743b0fded78fd538274542
Naii-the-Baf <sfabian200@gmail.com>
2024-12-03 19:56:50 -0600

    Add separators between item categories

--- gui/game_layout.py
+++ gui/game_layout.py
@@ -1,4 +1,5 @@
 from PySide6 import QtCore, QtWidgets, QtGui
+import game
 from gui.layouts.prime1_style import Prime1Style
 from gui.layouts.prime2_style import Prime2Style
 
@@ -6,12 +7,15 @@ class GameLayout(QtWidgets.QWidget):
     def __init__(self, world):
         super().__init__()
         self.game_style = None
+        self.game = None
 
         match world.game_id:
             case 'prime1':
                 self.game_style = Prime1Style()
+                self.game = game.Prime1()
             case 'prime2':
                 self.game_style = Prime2Style()
+                self.game = game.Prime2()
             case _:
                 self.game_style = None
 
@@ -22,11 +26,19 @@ class GameLayout(QtWidgets.QWidget):
         self.layout.setColumnStretch(1, 30)
         self.layout.setColumnStretch(2, 50)
         row_pos = 0
+        category = 0
         
         for item_category in item_locations:
             for item, locations in item_category.items():
                 if len(locations) == 0:
                     continue
+                
+                while (category < len(self.game.major_items)) and (item not in self.game.major_items[category]):
+                    category += 1
+                    separator = QtWidgets.QLabel("")
+                    self.layout.addWidget(separator, row_pos, 0, 1, 3)
+                    row_pos += 1
+                
                 text = QtWidgets.QLabel(item)
                 text.setStyleSheet("border:1px solid black;")
                 if world.game.victory_key in item:
@@ -41,5 +53,3 @@ class GameLayout(QtWidgets.QWidget):
                     text.setStyleSheet("background:" + self.game_style.background[location[0]] + ";color:" + self.game_style.foreground[location[0]] + ";")
                     self.layout.addWidget(text, row_pos, 2, 1, 1)
                     row_pos += 1
-
-
