6a79d267123657f92f0eb0bf90b4dc224b6abed5
Naii-the-Baf <sfabian200@gmail.com>
2024-12-09 17:29:47 -0600

    Add pre-commit Also applies changes from pre-commit

--- src/gui/notification_dialog.py
+++ src/gui/notification_dialog.py
@@ -1,25 +1,23 @@
-from PySide6 import QtCore, QtWidgets, QtGui
+from PySide6 import QtWidgets
 
-class NotificationDialog():
+
+class NotificationDialog:
     def __init__(self):
         pass
-    
+
     @staticmethod
-    def show(parent: QtWidgets.QWidget,
-             caption: str,
-             message: str):
+    def show(parent: QtWidgets.QWidget, caption: str, message: str):
         dialog = QtWidgets.QDialog(parent)
         dialog.setWindowTitle(caption)
-        
+
         dialog_layout = QtWidgets.QVBoxLayout()
         message_label = QtWidgets.QLabel(message)
         dialog_layout.addWidget(message_label)
-        
+
         button_values = QtWidgets.QDialogButtonBox.Close
         button_box = QtWidgets.QDialogButtonBox(button_values)
         button_box.rejected.connect(dialog.reject)
         dialog_layout.addWidget(button_box)
-        
+
         dialog.setLayout(dialog_layout)
         dialog.exec()
-        
--- src/main.py
+++ src/main.py
@@ -1,18 +1,18 @@
 import sys
 import signal
-from PySide6 import QtCore, QtWidgets, QtGui
+from PySide6 import QtWidgets
 from gui.main_window import MainWindow
 from arg_parser import ArgParser
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
-    
+
     parser = ArgParser()
     args = parser.get_args()
-    
+
     signal.signal(signal.SIGINT, signal.SIG_DFL)
     signal.signal(signal.SIGTERM, signal.SIG_DFL)
-    
+
     widget = MainWindow(args.file)
     widget.resize(600, 600)
     widget.show()
